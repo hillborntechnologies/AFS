6e803cb20acf07a42feba84bc0d63d61669df60d
hillborntechnologies <146350072+hillborntechnologies@users.noreply.github.com>
2023-10-13 22:48:49 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,232 @@
+jQuery(document).ready(function($){
+	//update these values if you change these breakpoints in the style.css file (or _layout.scss if you use SASS)
+	var MqM= 768,
+		MqL = 1024;
+
+	var faqsSections = $('.cd-faq-group'),
+		faqTrigger = $('.cd-faq-trigger'),
+		faqsContainer = $('.cd-faq-items'),
+		faqsCategoriesContainer = $('.cd-faq-categories'),
+		faqsCategories = faqsCategoriesContainer.find('a'),
+		closeFaqsContainer = $('.cd-close-panel');
+	
+	//select a faq section 
+	faqsCategories.on('click', function(event){
+		event.preventDefault();
+		var selectedHref = $(this).attr('href'),
+			target= $(selectedHref);
+		if( $(window).width() < MqM) {
+			faqsContainer.scrollTop(0).addClass('slide-in').children('ul').removeClass('selected').end().children(selectedHref).addClass('selected');
+			closeFaqsContainer.addClass('move-left');
+			$('body').addClass('cd-overlay');
+		} else {
+	        $('body,html').animate({ 'scrollTop': target.offset().top - 19}, 200); 
+		}
+	});
+
+	//close faq lateral panel - mobile only
+	$('body').bind('click touchstart', function(event){
+		if( $(event.target).is('body.cd-overlay') || $(event.target).is('.cd-close-panel')) { 
+			closePanel(event);
+		}
+	});
+	faqsContainer.on('swiperight', function(event){
+		closePanel(event);
+	});
+
+	//show faq content clicking on faqTrigger
+	faqTrigger.on('click', function(event){
+		event.preventDefault();
+		$(this).next('.cd-faq-content').slideToggle(200).end().parent('li').toggleClass('content-visible');
+	});
+
+	//update category sidebar while scrolling
+	$(window).on('scroll', function(){
+		if ( $(window).width() > MqL ) {
+			(!window.requestAnimationFrame) ? updateCategory() : window.requestAnimationFrame(updateCategory); 
+		}
+	});
+
+	$(window).on('resize', function(){
+		if($(window).width() <= MqL) {
+			faqsCategoriesContainer.removeClass('is-fixed').css({
+				'-moz-transform': 'translateY(0)',
+			    '-webkit-transform': 'translateY(0)',
+				'-ms-transform': 'translateY(0)',
+				'-o-transform': 'translateY(0)',
+				'transform': 'translateY(0)',
+			});
+		}	
+		if( faqsCategoriesContainer.hasClass('is-fixed') ) {
+			faqsCategoriesContainer.css({
+				'left': faqsContainer.offset().left,
+			});
+		}
+	});
+
+	function closePanel(e) {
+		e.preventDefault();
+		faqsContainer.removeClass('slide-in').find('li').show();
+		closeFaqsContainer.removeClass('move-left');
+		$('body').removeClass('cd-overlay');
+	}
+
+	function updateCategory(){
+		updateCategoryPosition();
+		updateSelectedCategory();
+	}
+
+	function updateCategoryPosition() {
+		var top = $('.cd-faq').offset().top,
+			height = jQuery('.cd-faq').height() - jQuery('.cd-faq-categories').height(),
+			margin = 20;
+		if( top - margin <= $(window).scrollTop() && top - margin + height > $(window).scrollTop() ) {
+			var leftValue = faqsCategoriesContainer.offset().left,
+				widthValue = faqsCategoriesContainer.width();
+			faqsCategoriesContainer.addClass('is-fixed').css({
+				'left': leftValue,
+				'top': margin,
+				'-moz-transform': 'translateZ(0)',
+			    '-webkit-transform': 'translateZ(0)',
+				'-ms-transform': 'translateZ(0)',
+				'-o-transform': 'translateZ(0)',
+				'transform': 'translateZ(0)',
+			});
+		} else if( top - margin + height <= $(window).scrollTop()) {
+			var delta = top - margin + height - $(window).scrollTop();
+			faqsCategoriesContainer.css({
+				'-moz-transform': 'translateZ(0) translateY('+delta+'px)',
+			    '-webkit-transform': 'translateZ(0) translateY('+delta+'px)',
+				'-ms-transform': 'translateZ(0) translateY('+delta+'px)',
+				'-o-transform': 'translateZ(0) translateY('+delta+'px)',
+				'transform': 'translateZ(0) translateY('+delta+'px)',
+			});
+		} else { 
+			faqsCategoriesContainer.removeClass('is-fixed').css({
+				'left': 0,
+				'top': 0,
+			});
+		}
+	}
+
+	function updateSelectedCategory() {
+		faqsSections.each(function(){
+			var actual = $(this),
+				margin = parseInt($('.cd-faq-title').eq(1).css('marginTop').replace('px', '')),
+				activeCategory = $('.cd-faq-categories a[href="#'+actual.attr('id')+'"]'),
+				topSection = (activeCategory.parent('li').is(':first-child')) ? 0 : Math.round(actual.offset().top);
+			
+			if ( ( topSection - 20 <= $(window).scrollTop() ) && ( Math.round(actual.offset().top) + actual.height() + margin - 20 > $(window).scrollTop() ) ) {
+				activeCategory.addClass('selected');
+			}else {
+				activeCategory.removeClass('selected');
+			}
+		});
+	}
+});
+
+
+const loanAmountInput = document.querySelector(".loan-amount");
+const interestRateInput = document.querySelector(".interest-rate");
+const loanTenureInput = document.querySelector(".loan-tenure");
+
+const loanEMIValue = document.querySelector(".loan-emi .value");
+const totalInterestValue = document.querySelector(".total-interest .value");
+const totalAmountValue = document.querySelector(".total-amount .value");
+
+const calculateBtn = document.querySelector(".calculate-btn");
+
+let loanAmount = parseFloat(loanAmountInput.value);
+let interestRate = parseFloat(interestRateInput.value);
+let loanTenure = parseFloat(loanTenureInput.value);
+
+let interest = interestRate / 12 / 100;
+
+let myChart;
+
+const checkValues = () => {
+  let loanAmountValue = loanAmountInput.value;
+  let interestRateValue = interestRateInput.value;
+  let loanTenureValue = loanTenureInput.value;
+
+  let regexNumber = /^[0-9]+$/;
+  if (!loanAmountValue.match(regexNumber)) {
+    loanAmountInput.value = "10000";
+  }
+
+  if (!loanTenureValue.match(regexNumber)) {
+    loanTenureInput.value = "12";
+  }
+
+  let regexDecimalNumber = /^(\d*\.)?\d+$/;
+  if (!interestRateValue.match(regexDecimalNumber)) {
+    interestRateInput.value = "7.5";
+  }
+};
+
+const displayChart = (totalInterestPayableValue) => {
+  const ctx = document.getElementById("myChart").getContext("2d");
+  myChart = new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: ["Total Interest", "Principal Loan Amount"],
+      datasets: [
+        {
+          data: [totalInterestPayableValue, loanAmount],
+          backgroundColor: ["#FCC201", "#000000"],
+          borderWidth: 0,
+        },
+      ],
+    },
+  });
+};
+
+const updateChart = (totalInterestPayableValue) => {
+  myChart.data.datasets[0].data[0] = totalInterestPayableValue;
+  myChart.data.datasets[0].data[1] = loanAmount;
+  myChart.update();
+};
+
+const refreshInputValues = () => {
+  loanAmount = parseFloat(loanAmountInput.value);
+  interestRate = parseFloat(interestRateInput.value);
+  loanTenure = parseFloat(loanTenureInput.value);
+  interest = interestRate / 12 / 100;
+};
+
+const calculateEMI = () => {
+  checkValues();
+  refreshInputValues();
+  let emi =
+    loanAmount *
+    interest *
+    (Math.pow(1 + interest, loanTenure) /
+      (Math.pow(1 + interest, loanTenure) - 1));
+
+  return emi;
+};
+
+const updateData = (emi) => {
+  loanEMIValue.innerHTML = Math.round(emi);
+
+  let totalAmount = Math.round(loanTenure * emi);
+  totalAmountValue.innerHTML = totalAmount;
+
+  let totalInterestPayable = Math.round(totalAmount - loanAmount);
+  totalInterestValue.innerHTML = totalInterestPayable;
+
+  if (myChart) {
+    updateChart(totalInterestPayable);
+  } else {
+    displayChart(totalInterestPayable);
+  }
+};
+
+const init = () => {
+  let emi = calculateEMI();
+  updateData(emi);
+};
+
+init();
+
+calculateBtn.addEventListener("click", init);
